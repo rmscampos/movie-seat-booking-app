eca2cf80bc0139ead92aa4d1a3405071b247e515
Regina Campos <rmscampos@gmail.com>
2020-12-10 19:19:37 -0600

    saves data to local storage

--- main.js
+++ main.js
@@ -4,13 +4,26 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = parseInt(movieSelect.value);
+let ticketPrice = parseInt(movieSelect.value);
 // does the same as parseInt
 // const ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
 
 //update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  // Copy selected seats into an array
+  // Map through array
+  // Return a new array of indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -18,6 +31,14 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event 
 container.addEventListener('click', (e) => {
   if (
     e.target.classList.contains('seat') && 
